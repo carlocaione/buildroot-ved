f7b098a3a87f16215659c4aab1c8fd936fd74f20
Carlo Caione <carlo@caione.org>
2014-05-18 14:08:22 +0200

    Add mono 3.4.0 package
    Signed-off-by: Carlo Caione <carlo@caione.org>

--- package/mono/mono-managed/mono-managed.mk
+++ package/mono/mono-managed/mono-managed.mk
@@ -4,25 +4,10 @@
 #
 #############################################################
 
-MONO_MANAGED_VERSION         = $(MONO_VERSION)
-MONO_MANAGED_SITE            = $(MONO_SITE)
-MONO_MANAGED_SOURCE 	     = $(MONO_SOURCE)
+MONO_MANAGED_VERSION = $(MONO_VERSION)
+MONO_MANAGED_SITE = $(MONO_SITE)
+MONO_MANAGED_SOURCE = $(MONO_SOURCE)
 MONO_MANAGED_INSTALL_STAGING = $(MONO_INSTALL_STAGING)
+HOST_MONO_MANAGED_CONF_OPT += --enable-static --prefix=$(TARGET_DIR)/usr
 
-MONO_MANAGED_CONF_ENV = \
-	CFLAGS="--sysroot=$(STAGING_DIR)"
-
-define MONO_MANAGED_CONFIGURE_CMDS
-	(cd $(@D); \
-		PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)" \
-		PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" \
-		MAKEFLAGS="$(MAKEFLAGS) -j$(PARALLEL_JOBS)" ./configure \
-		--with-sysroot=$(STAGING_DIR) \
-	)
-endef
-
-define MONO_MANAGED_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
-endef
-
-$(eval $(generic-package))
+$(eval $(host-autotools-package))
--- package/mono/mono.mk
+++ package/mono/mono.mk
@@ -9,15 +9,6 @@ MONO_SITE	= http://download.mono-project.com/sources/mono/
 MONO_SOURCE	= mono-$(MONO_VERSION).tar.bz2
 MONO_INSTALL_STAGING = YES
 
-MONO_CONF_OPT = \
-	--program-prefix="" \
-	--disable-gtk-doc \
-	--enable-static \
-	--enable-shared \
-	--with-tls=pthread \
-	--with-sigaltstack=no \
-	--with-mcs-docs=no 
-
 ifeq ($(BR2_PACKAGE_MONO),y)
 	include package/mono/*/*.mk
 	TARGETS += mono-managed mono-native
